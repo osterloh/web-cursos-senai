f9c3e843463f693122f4a83e12872d0ecdca618c
Johnatan <johnatanosterloh@gmail.com>
2022-01-03 16:15:02 -0300

    Refatoração do nome do componente e importação da footer

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 
 import Header from "../Header";
+import Footer from "../Footer";
 
 import { Container } from "./styles";
 
-const Footer: React.FC = () => {
+const Dashboard: React.FC = () => {
   return (
     <>
       <Container>
         <Header />
         <h1>hello</h1>
+        <Footer />
       </Container>
     </>
   );
 };
 
-export default Footer;
+export default Dashboard;
