503ad931c4cfb81267963ed19753ffd000272828
Johnatan <johnatanosterloh@gmail.com>
2022-01-03 17:00:34 -0300

    Configurações iniciais da Footer e estilização

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,73 @@
 import React from "react";
 
+import logoSenai from "../../assets/logo.svg";
 import { Container } from "./styles";
 
 const Footer: React.FC = () => {
   return (
     <>
-      <Container>Hello</Container>
+      <Container>
+        <div id="linksInternos">
+          <div id="sobre">
+            <p>SOBRE O SENAI</p>
+            <a href="#">Sobre o SENAI</a>
+            <a href="#">Diretoria</a>
+            <a href="#">Nossas Unidades</a>
+            <a href="#">Lei de Diretrizes Orçamentarias</a>
+            <a href="#">Politica de Privacidade</a>
+            <a href="#">Entidades</a>
+            <a href="#">Imprensa</a>
+          </div>
+          <div id="areas">
+            <p>ÁREAS DE ATUAÇÃO</p>
+            <a href="#">Educação</a>
+            <a href="#">Tecnologia e Inovação</a>
+            <a href="#">Faculdade SENAI</a>
+            <a href="#">Blog SESI SENAI</a>
+          </div>
+          <div id="paginas">
+            <p>OUTRAS PÁGINAS</p>
+            <a href="#">Descontos, Bolsas e Financiamento</a>
+            <a href="#">Trabalhe Conosco</a>
+            <a href="#">Portal do Fornecedor</a>
+            <a href="#">Intranet</a>
+            <a href="#">Compliance</a>
+            <a href="#">Transparência</a>
+            <a href="#">Prestação de Contas TCU</a>
+          </div>
+          <div id="federacao">
+            <p>FEDERAÇÃO DAS INDÚSTRIAS DO ESTADO DE SANTA CATARINA</p>
+            <div>
+              <img src="" alt="" />
+              <div>
+                <p>Rod. Admar Gonzaga, 2765</p>
+                <p>Florianópolis/SC - 88034-001</p>
+              </div>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <p>faleconosco@fiesc.com.br</p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <div>
+                <p>(48) 3231 4100</p>
+                <p>0800 048 1212</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="midiasSociais">
+          <img src={logoSenai} alt="SENAI" />
+          <div>
+            <a href="#">facebook</a>
+            <a href="#">twiter</a>
+            <a href="#">instagram</a>
+            <a href="#">linkedin</a>
+            <a href="#">youtube</a>
+          </div>
+        </div>
+      </Container>
     </>
   );
 };
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,5 +1,46 @@
 import styled from "styled-components";
 
 export const Container = styled.h1`
-  color: #f3b8;
+  background-color: #006bb7;
+
+  #linksInternos {
+    max-width: 100%;
+    width: 1140px;
+
+    margin-left: auto;
+    margin-right: auto;
+
+    border-bottom: 1px solid #fff;
+
+    display: flex;
+    justify-content: space-between;
+
+    div {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      /* padding: 1.25rem 0; */
+      width: 280px;
+      font-size: 16px;
+
+      p {
+        font-weight: bold;
+        font-size: 0.9em;
+        color: #fff;
+        width: 100%;
+        display: block;
+        padding: 1.25rem 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: #fff;
+        font-weight: 300;
+        font-size: 0.875em;
+        line-height: 1.5;
+
+        width: 100%;
+      }
+    }
+  }
 `;
