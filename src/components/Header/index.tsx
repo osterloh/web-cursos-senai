3f1d39264dc1e3032c9839a39772705a5820856f
Johnatan <johnatanosterloh@gmail.com>
2022-01-03 16:10:15 -0300

    Configuração da Header do projeto e estilização

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+  faLinkedinIn,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
+import logo from "../../assets/logo.svg";
 import { Container } from "./styles";
 
-const Footer: React.FC = () => {
+const Header: React.FC = () => {
   return (
     <>
       <Container>
@@ -10,9 +21,65 @@ const Footer: React.FC = () => {
           Quer receber nosso conteúdo exclusivo?{" "}
           <a href="https://fiesc.com.br/inscricoes">Inscreva-se!</a>
         </div>
+        <div className="linksInstitucionais">
+          <div id="linkCasas">
+            <a href="https://fiesc.com.br/">FIESC</a>
+            <a href="https://www.ciesc.com.br/">CIESC</a>
+            <a href="https://sesisc.org.br/">SESI</a>
+            <a href="https://sc.senai.br/">SENAI</a>
+            <a href="https://ielsc.org.br/">IEL</a>
+          </div>
+          <div id="bannerSenai">
+            <a href="https://sc.senai.br/">
+              <img src={logo} alt="logoSenai" />
+            </a>
+          </div>
+          <div id="linksMidias">
+            <div id="redes">
+              {library.add(
+                faFacebookF,
+                faTwitter,
+                faInstagram,
+                faLinkedinIn,
+                faYoutube
+              )}
+              <a href="https://www.facebook.com/senaisc">
+                <FontAwesomeIcon icon={["fab", "facebook-f"]} />
+              </a>
+              <a href="https://twitter.com/SENAISC">
+                <FontAwesomeIcon icon={["fab", "twitter"]} />
+              </a>
+              <a href="https://www.instagram.com/senai.sc/">
+                <FontAwesomeIcon icon={["fab", "instagram"]} />
+              </a>
+              <a href="https://www.linkedin.com/school/senai-sc/mycompany/">
+                <FontAwesomeIcon icon={["fab", "linkedin-in"]} />
+              </a>
+              <a href="https://www.youtube.com/senaiconhecimento">
+                <FontAwesomeIcon icon={["fab", "youtube"]} />
+              </a>
+            </div>
+            <div id="internos">
+              <a href="https://sc.senai.br/compliance">Compliance</a>
+              <a href="#">Transparência</a>
+              <a href="https://intranet.sesisc.org.br/intranet/php/login.php?">
+                Intranet
+              </a>
+            </div>
+          </div>
+        </div>
+        <div id="linkSobre">
+          <a href="https://sc.senai.br/sobre-senai">SOBRE O SENAI</a>
+          <a href="https://sc.senai.br/educacao">EDUCAÇÃO</a>
+          <a href="https://sc.senai.br/tecnologia-e-inovacao">
+            TECNOLOGIA E INOVAÇÃO
+          </a>
+          <a href="https://sc.senai.br/faculdade-senai">FACULDADE SENAI</a>
+          <a href="https://blog.sesisenai.org.br/">BLOG SESI SENAI</a>
+        </div>
       </Container>
     </>
   );
 };
 
-export default Footer;
+export default Header;
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -21,11 +21,135 @@ export const Container = styled.div`
     overflow-x: hidden;
 
     a {
-      color: #006bb7;
       text-decoration: none;
+      border-bottom: 1px solid;
       padding: 0.15em 0.1em;
-      border-bottom: 1px solid #006bb7;
       transition: all 0.1s ease;
+
+      &:hover {
+        color: #006bb7;
+        border-bottom: 1px solid #006bb7;
+        opacity: 1;
+      }
+    }
+  }
+
+  .linksInstitucionais {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    padding: 0px 60px;
+
+    #linkCasas {
+      font-weight: 300;
+
+      a {
+        font-family: MuseoSans, Arial, sans-serif;
+        font-size: 16px;
+        text-decoration: none;
+        text-transform: uppercase;
+        font-weight: bold;
+        padding: 0px 10px;
+        color: #5b5b5b;
+        opacity: 0.8;
+
+        transition: 0.2s;
+
+        &:hover {
+          color: #006bb7;
+          opacity: 1;
+        }
+      }
+    }
+
+    #bannerSenai {
+      a {
+        text-decoration: none;
+        img {
+          width: 200px;
+        }
+      }
+    }
+
+    #linksMidias {
+      #redes {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex-end;
+
+        a {
+          text-decoration: none;
+          color: #006bb7;
+          font-size: 18px;
+          min-height: 30px;
+          padding: 0 8px;
+        }
+      }
+
+      #internos {
+        padding: 5px 0 5px 0;
+        margin: 0;
+        font-weight: 300;
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+
+        a {
+          color: #5b5b5b;
+          text-decoration: none;
+          font-family: MuseoSans, Arial, sans-serif;
+          line-height: 1.4em;
+          font-weight: bold;
+
+          margin-left: 1.3em;
+
+          transition: 0.2s;
+
+          &:hover {
+            color: #006bb7;
+            opacity: 1;
+          }
+        }
+      }
+    }
+  }
+
+  #linkSobre {
+    font-size: 16px;
+    line-height: 1.4;
+    font-family: MuseoSans, Arial, sans-serif;
+    font-weight: 300;
+
+    max-height: 40em;
+    background-color: #006bb7;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 44px;
+
+    a {
+      text-decoration: none;
+      color: rgb(236, 236, 236);
+      font-weight: 700;
+      font-size: 10.4px;
+      padding: 0 1em;
+
+      height: 100%;
+
+      display: flex;
+      align-items: center;
+
+      transition: 0.2s;
+
+      &:hover {
+        background-color: #219ce1;
+      }
     }
   }
 `;
