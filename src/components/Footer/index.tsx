e1bc1860ef1b826cd65e119a5c8bc6d957a86eba
Johnatan <johnatanosterloh@gmail.com>
2022-01-04 13:58:17 -0300

    Configuração e estilização da Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 
-import logoSenai from "../../assets/logo.svg";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faInstagram,
+  faLinkedinIn,
+  faYoutube,
+  faFacebookF,
+} from "@fortawesome/free-brands-svg-icons";
+
+import logoSenai from "../../assets/logo2.svg";
+import emailSenai from "../../assets/email.png";
+import phoneSenai from "../../assets/phone.png";
+import mapSenai from "../../assets/map.png";
 import { Container } from "./styles";
 
 const Footer: React.FC = () => {
@@ -10,61 +23,112 @@ const Footer: React.FC = () => {
         <div id="linksInternos">
           <div id="sobre">
             <p>SOBRE O SENAI</p>
-            <a href="#">Sobre o SENAI</a>
-            <a href="#">Diretoria</a>
-            <a href="#">Nossas Unidades</a>
-            <a href="#">Lei de Diretrizes Orçamentarias</a>
-            <a href="#">Politica de Privacidade</a>
-            <a href="#">Entidades</a>
-            <a href="#">Imprensa</a>
+            <div>
+              <a href="https://sc.senai.br/sobre-senai">Sobre o SENAI</a>
+              <a href="https://sc.senai.br/diretoria">Diretoria</a>
+              <a href="https://sc.senai.br/unidades-senai">Nossas Unidades</a>
+              <a href="https://transparencia.sc.senai.br/lei-de-diretrizes-orcamentarias">
+                Lei de Diretrizes Orçamentarias
+              </a>
+              <a href="https://sc.senai.br/politica-de-privacidade">
+                Politica de Privacidade
+              </a>
+              <a href="https://sc.senai.br/entidades">Entidades</a>
+              <a href="https://sc.senai.br/imprensa">Imprensa</a>
+            </div>
           </div>
           <div id="areas">
             <p>ÁREAS DE ATUAÇÃO</p>
-            <a href="#">Educação</a>
-            <a href="#">Tecnologia e Inovação</a>
-            <a href="#">Faculdade SENAI</a>
-            <a href="#">Blog SESI SENAI</a>
+            <div>
+              <a href="https://sc.senai.br/educacao">Educação</a>
+              <a href="https://sc.senai.br/tecnologia-e-inovacao">
+                Tecnologia e Inovação
+              </a>
+              <a href="https://sc.senai.br/faculdade-senai">Faculdade SENAI</a>
+              <a href="https://blog.sesisenai.org.br/">Blog SESI SENAI</a>
+            </div>
           </div>
           <div id="paginas">
             <p>OUTRAS PÁGINAS</p>
-            <a href="#">Descontos, Bolsas e Financiamento</a>
-            <a href="#">Trabalhe Conosco</a>
-            <a href="#">Portal do Fornecedor</a>
-            <a href="#">Intranet</a>
-            <a href="#">Compliance</a>
-            <a href="#">Transparência</a>
-            <a href="#">Prestação de Contas TCU</a>
+            <div>
+              <a href="https://sc.senai.br/descontos">
+                Descontos, Bolsas e Financiamento
+              </a>
+              <a href="https://www.crescemosjuntos.com.br/trabalhe-conosco">
+                Trabalhe Conosco
+              </a>
+              <a href="https://portaldecompras.fiesc.com.br/Default.aspx">
+                Portal do Fornecedor
+              </a>
+              <a href="https://intranet.sesisc.org.br/intranet">Intranet</a>
+              <a href="https://sc.senai.br/compliance">Compliance</a>
+              <a href="https://transparencia.sc.senai.br/">Transparência</a>
+              <a href="https://transparencia.sc.senai.br/prestacao-de-contas-tcu">
+                Prestação de Contas TCU
+              </a>
+            </div>
           </div>
           <div id="federacao">
             <p>FEDERAÇÃO DAS INDÚSTRIAS DO ESTADO DE SANTA CATARINA</p>
             <div>
-              <img src="" alt="" />
               <div>
-                <p>Rod. Admar Gonzaga, 2765</p>
-                <p>Florianópolis/SC - 88034-001</p>
+                <img src={mapSenai} alt="Map" />
+                <div>
+                  <p>Rod. Admar Gonzaga, 2765</p>
+                  <p>Florianópolis/SC - 88034-001</p>
+                </div>
               </div>
-            </div>
-            <div>
-              <img src="" alt="" />
-              <p>faleconosco@fiesc.com.br</p>
-            </div>
-            <div>
-              <img src="" alt="" />
               <div>
-                <p>(48) 3231 4100</p>
-                <p>0800 048 1212</p>
+                <img src={emailSenai} alt="E-Mail" />
+                <div>
+                  <p>faleconosco@fiesc.com.br</p>
+                </div>
+              </div>
+              <div>
+                <img src={phoneSenai} alt="Phone" />
+                <div>
+                  <p>(48) 3231 4100</p>
+                  <p>0800 048 1212</p>
+                </div>
+              </div>
+              <div>
+                <div id="faleConosco">
+                  <a href="https://transparencia.sc.senai.br/fale-conosco?entidade=senai">
+                    FALE CONOSCO
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div id="midiasSociais">
-          <img src={logoSenai} alt="SENAI" />
+          <a href="#">
+            <img src={logoSenai} alt="SENAI" />
+          </a>
           <div>
-            <a href="#">facebook</a>
-            <a href="#">twiter</a>
-            <a href="#">instagram</a>
-            <a href="#">linkedin</a>
-            <a href="#">youtube</a>
+            {library.add(
+              faFacebookF,
+              faTwitter,
+              faInstagram,
+              faLinkedinIn,
+              faYoutube
+            )}
+            <a href="https://www.facebook.com/senaisc">
+              <FontAwesomeIcon icon={["fab", "facebook-f"]} />
+            </a>
+
+            <a href="https://twitter.com/SENAISC">
+              <FontAwesomeIcon icon={["fab", "twitter"]} />
+            </a>
+            <a href="https://www.instagram.com/senai.sc/">
+              <FontAwesomeIcon icon={["fab", "instagram"]} />
+            </a>
+            <a href="https://www.linkedin.com/school/senai-sc/mycompany/">
+              <FontAwesomeIcon icon={["fab", "linkedin-in"]} />
+            </a>
+            <a href="https://www.youtube.com/senaiconhecimento">
+              <FontAwesomeIcon icon={["fab", "youtube"]} />
+            </a>
           </div>
         </div>
       </Container>
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.h1`
+export const Container = styled.div`
   background-color: #006bb7;
 
   #linksInternos {
@@ -9,17 +9,20 @@ export const Container = styled.h1`
 
     margin-left: auto;
     margin-right: auto;
+    margin-bottom: 55px;
 
     border-bottom: 1px solid #fff;
 
     display: flex;
     justify-content: space-between;
+    align-items: flex-start;
 
     div {
       display: flex;
       flex-wrap: wrap;
       justify-content: space-between;
-      /* padding: 1.25rem 0; */
+      align-items: flex-start;
+      padding: 1.25rem 0;
       width: 280px;
       font-size: 16px;
 
@@ -32,14 +35,108 @@ export const Container = styled.h1`
         padding: 1.25rem 0;
       }
 
+      div {
+        a {
+          text-decoration: none;
+          color: #fff;
+          font-weight: 300;
+          font-size: 0.875em;
+          line-height: 1.5;
+
+          width: 100%;
+        }
+
+        div {
+          display: flex;
+          align-items: center;
+          justify-content: space-around;
+
+          margin: -0.5rem 0;
+
+          img {
+            width: 50px;
+          }
+          div {
+            width: 80%;
+
+            p {
+              margin: -1.25rem 0;
+              font-weight: 300;
+            }
+          }
+
+          #faleConosco {
+            text-align: center;
+
+            a {
+              font-weight: bold;
+              text-decoration: none;
+              border: 2px solid #fff;
+              width: 180px;
+              height: 39px;
+              margin: 0 0.5em 0.5em 0;
+
+              display: flex;
+              align-items: center;
+              justify-content: center;
+
+              transition: all 0.5s ease-out;
+
+              &:hover {
+                background-color: #fff;
+                color: #006bb7;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  #midiasSociais {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    flex-direction: row;
+    justify-content: space-around;
+
+    a {
+      text-decoration: none;
+      margin-bottom: 50px;
+
+      img {
+        width: 190px;
+      }
+    }
+
+    div {
+      margin-bottom: 50px;
+      margin-top: 5px;
+
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      padding: 10px 0 5px 0;
+
       a {
         text-decoration: none;
-        color: #fff;
-        font-weight: 300;
-        font-size: 0.875em;
-        line-height: 1.5;
+        margin: 0.5em;
 
-        width: 100%;
+        svg {
+          width: 1em;
+          color: #006bb7;
+          background-color: #fff;
+          border: solid 2px #fff;
+          border-radius: 50%;
+          text-align: center;
+
+          transition: all 0.3s ease;
+
+          &:hover {
+            color: #fff;
+            background-color: #006bb7;
+          }
+        }
       }
     }
   }
