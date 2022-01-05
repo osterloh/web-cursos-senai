7e2726ad412c149655e210a6c0729fb2677153e6
Johnatan <johnatanosterloh@gmail.com>
2022-01-05 10:15:33 -0300

    Configuração da rota para Appointment

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Dashboard from "../components/Dashboard";
+import Appointment from "../components/Appointment";
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
+    <Route path="/appointment" component={Appointment} />
   </Switch>
 );
 
