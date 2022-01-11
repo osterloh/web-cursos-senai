4271714d7f3f43e8d95974394cbf8b87167635f6
Johnatan <johnatanosterloh@gmail.com>
2022-01-11 18:21:17 -0300

    Configuração e estilização inicial do componente Update

---
+++ src/components/Update/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Update: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <h2>Atualização de dados para demanda</h2>
+      </Container>
+    </>
+  );
+};
+
+export default Update;
---
+++ src/components/Update/styles.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background: #b4b4d5;
+`;
