a7750a359e40858f1d7d62408955c713f63d3f3b
Johnatan <johnatanosterloh@gmail.com>
2022-01-11 18:27:19 -0300

    Remoção de components staticos e conexão com a API

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../../service/api";
+import { Link } from "react-router-dom";
 
 import Header from "../Header";
 import Footer from "../Footer";
@@ -6,27 +8,46 @@ import Card from "../Card";
 
 import { Container } from "./styles";
 
+interface ICurso {
+  id: number;
+  curso: string;
+  totalDeVagas: number;
+  vagasDisponiveis: number;
+  vagasDisponiveisAte: Date;
+}
+
 const Dashboard: React.FC = () => {
+  const [cursos, setCurso] = useState<ICurso[]>([]);
+
+  useEffect(() => {
+    api.get("/curso").then((response) => {
+      setCurso(response.data);
+    });
+  }, []);
+
+  console.log(cursos);
+
   return (
     <>
       <Header />
       <Container>
-        <h2>
-          Cursos de Aprendizagem Industrial e Quantidade de Vagas Disponíveis
-          para a Indústria
-        </h2>
+        <div id="cabecalho">
+          <h2>
+            Cursos de Aprendizagem Industrial e Quantidade de Vagas Disponíveis
+            para a Indústria
+          </h2>
+          <Link to={"/login"}>Manutenção de Demandas</Link>
+        </div>
         <div id="main">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {cursos.map((curso) => (
+            <Card
+              key={curso.id}
+              curso={curso.curso}
+              totalDeVagas={curso.totalDeVagas}
+              vagasDisponiveis={curso.vagasDisponiveis}
+              vagasDisponiveisAte={curso.vagasDisponiveisAte}
+            />
+          ))}
         </div>
       </Container>
       <Footer />
--- src/components/Dashboard/styles.ts
+++ src/components/Dashboard/styles.ts
@@ -1,12 +1,37 @@
 import styled from "styled-components";
+import { shade } from "polished";
 
 export const Container = styled.div`
-  h2 {
-    text-align: center;
-    margin: 10px;
-    padding: 10px;
+  #cabecalho {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
 
     border-bottom: 1px solid #006bb7;
+
+    h2 {
+      text-align: center;
+      margin: 10px;
+      padding: 10px;
+    }
+
+    a {
+      text-decoration: none;
+      font-size: 14px;
+      color: #fff;
+      background-color: #006bb7;
+      border: 1px solid #006bb7;
+      border-radius: 4px;
+      width: 120px;
+      text-align: center;
+      padding: 3px;
+
+      transition: all 0.5s ease-out;
+
+      &:hover {
+        background-color: ${shade(0.2, "#006bb7")};
+      }
+    }
   }
   #main {
     min-width: 95%;
