f92b8e4a1908e044dd6a7cec8e3ece78c2753d0f
Johnatan <johnatanosterloh@gmail.com>
2022-01-11 18:15:28 -0300

    Adicionado rotas de Login e Demanda

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,15 @@ import { Switch, Route } from "react-router-dom";
 
 import Dashboard from "../components/Dashboard";
 import Appointment from "../components/Appointment";
+import Login from "../components/Login";
+import Demanda from "../components/Demanda";
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
     <Route path="/appointment" component={Appointment} />
+    <Route path="/login" component={Login} />
+    <Route path="/demanda" component={Demanda} />
   </Switch>
 );
 
